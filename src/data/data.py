6f725b7d32b663d09a4c6b27157ddff45d24f01c
Bryant Juspi <bjuspi@outlook.com>
2021-07-10 09:22:50 +0800

    added interim data generation script

--- src/data/data.py
+++ src/data/data.py
@@ -1,12 +1,15 @@
-import os, sys
+import os
 import pandas as pd
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 rootdir = os.path.dirname(parentdir)
 
-y = r"\data\raw\AMZN\price.xlsx"
-pricedir = rootdir + y
-x = pd.read_excel(pricedir)
+df = pd.read_excel(rootdir + r"\data\raw\AMZN\price.xlsx")
 
-print("hello")
+header = df.iloc[0]
+df = df[1:]
+df.columns = header
+df["Dates"] = df.index
+
+df.to_csv(rootdir + r"\data\interim\AMZN\price.csv")
