ed68d374363bafdd53c954eb9c4cc5dbde1730a3
Bryant Juspi <bjuspi@outlook.com>
2021-07-10 09:50:51 +0800

    added os independent way of generating interim price data

--- src/data/data.py
+++ src/data/data.py
@@ -1,15 +1,26 @@
 import os
+import re
 import pandas as pd
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 rootdir = os.path.dirname(parentdir)
+datadir = os.path.join(rootdir, "data")
+rawdatadir = os.path.join(datadir, "raw")
 
-df = pd.read_excel(rootdir + r"\data\raw\AMZN\price.xlsx")
+for root, subdirs, files in os.walk(rawdatadir):
+    print('--\nroot = ' + root)
+    print(subdirs)
+    print(files)
 
-header = df.iloc[0]
-df = df[1:]
-df.columns = header
-df["Dates"] = df.index
+    if "price.xlsx" in files:
+        company = os.path.basename(root)
 
-df.to_csv(rootdir + r"\data\interim\AMZN\price.csv")
+        df = pd.read_excel(os.path.join(root, "price.xlsx"))
+        
+        header = df.iloc[0]
+        df = df[1:]
+        df.columns = header
+        df["Dates"] = df.index
+
+        df.to_csv(os.path.join(os.path.join(os.path.join(datadir, "interim"), company), "price.csv"))
